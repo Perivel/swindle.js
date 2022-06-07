d212dcc66712a4e63642189094c54c69de48dda8
PatrickLumenus <patrickaluy@gmail.com>
2022-06-06 20:47:20 -0400

    Added System module

---
+++ packages/system/__tests__/system.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const system = require('..');
+
+describe('system', () => {
+    it('needs tests');
+});
---
+++ packages/system/lib/system.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = system;
+
+function system() {
+    // TODO
+}
