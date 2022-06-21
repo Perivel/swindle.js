ceb1befd77cd7a3ac3ee03e2a27dd3fcdec676ba
PatrickLumenus <patrickaluy@gmail.com>
2022-06-21 04:03:40 -0400

    Updated Jest Config to support ESM

--- packages/structs/jest.config.cjs
+++ packages/structs/jest.config.cjs
@@ -5,4 +5,13 @@ module.exports = {
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  globals: {
+    "ts-jet": {
+      useESM: true
+    }
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  preset: 'ts-jest/presets/default-esm'
 }
